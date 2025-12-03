972bc5bd03862368809d5ca9faeb1d054e93ef0b
Omar-Ghareeb <omarghareeb2005@gmail.com>
2025-12-03 20:30:12 +0200

    adding loop to menu

--- gameMenu.cpp
+++ gameMenu.cpp
@@ -3,64 +3,69 @@ using namespace std;
 
 int main()
 {
-    vector<string> games = {"Infinity_Tic", "SUS", "Word", "Memory_XO", "Numerical_XO", "4x4_XO", "Pyramid_XO",
-                            "Obstacles_XO", "Four in a row", "Five X Five tic tac toe", "Misere Tic Tac Toe", "Diamond Tic Tac Toe", "Ultimate tic tac toe"};
+    bool flag = true;
 
-    cout << "choose a game" << '\n';
+    while (flag)
+    {
+        vector<string> games = {"Infinity_Tic", "SUS", "Word", "Memory_XO", "Numerical_XO", "4x4_XO", "Pyramid_XO",
+                                "Obstacles_XO", "Four in a row", "Five X Five tic tac toe", "Misere Tic Tac Toe", "Diamond Tic Tac Toe", "Ultimate tic tac toe"};
 
-    for (int i = 0; i < games.size(); i++)
-    {
-        cout << i + 1 << " " << games[i] << '\n';
-    }
-    cout << "0 to exit\n";
-    int pick;
-    cin >> pick;
-    switch (pick)
-    {
-    case 0:
-        break;
-    case 1:
-        runInfinity();
-        break;
-    case 2:
-        runSUS();
-        break;
-    case 3:
-        runWord();
-        break;
-    case 4:
-        runMemoryXO();
-        break;
-    case 5:
-        runNumerical();
-        break;
-    case 6:
-        run4x4XO();
-        break;
-    case 7:
-        runPyramidXO();
-        break;
-    case 8:
-        runInObstacles();
-        break;
-    case 9:
-        runFouInARow();
-        break;
-    case 10:
-        run5X5();
-        break;
-    case 11:
-        runMisere();
-        break;
-    case 12:
-        runDiamond();
-        break;
-    case 13:
-        runUltimate();
-        break;
-    default:
-        cout << "try again" << '\n';
-        break;
+        cout << "choose a game" << '\n';
+
+        for (int i = 0; i < games.size(); i++)
+        {
+            cout << i + 1 << " " << games[i] << '\n';
+        }
+        cout << "0 to exit\n";
+        int pick;
+        cin >> pick;
+        switch (pick)
+        {
+        case 0:
+            break;
+        case 1:
+            runInfinity();
+            break;
+        case 2:
+            runSUS();
+            break;
+        case 3:
+            runWord();
+            break;
+        case 4:
+            runMemoryXO();
+            break;
+        case 5:
+            runNumerical();
+            break;
+        case 6:
+            run4x4XO();
+            break;
+        case 7:
+            runPyramidXO();
+            break;
+        case 8:
+            runInObstacles();
+            break;
+        case 9:
+            runFouInARow();
+            break;
+        case 10:
+            run5X5();
+            break;
+        case 11:
+            runMisere();
+            break;
+        case 12:
+            runDiamond();
+            break;
+        case 13:
+            runUltimate();
+            break;
+        default:
+            cout << "try again" << '\n';
+            break;
+        }
     }
     return 0;
 }
