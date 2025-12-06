8dacaed14cc0b025b693191393bcf0da115bec0d
EmanHosny587 <emanhosny492@gmail.com>
2025-12-06 20:24:12 +0200

    docs(doxygen): add detailed comments for my classes and member functions

--- games/Four_in_a_row/Four_in_a_row_UI.h
+++ games/Four_in_a_row/Four_in_a_row_UI.h
@@ -1,15 +1,42 @@
 #pragma once
 #include "BoardGame_Classes.h"
 
+
+/**
+ * @class Four_in_a_row_UI
+ * @brief User Interface handler for the four_in_a_row game.
+ * @details Handles player creation and move input for the 3x3 four_in_a_row board.
+ * @see UI
+ */
 class Four_in_a_row_UI : public UI<char>
 {
 private:
-    //to check the available moves in each column
-    // available[column]=row
-    int available[7] = {5, 5, 5, 5, 5, 5, 5};
+    int available[7] = {5, 5, 5, 5, 5, 5, 5};///<checks the available move in each column.
 
 public:
+
+    /**
+    * @brief Constructs Four_in_a_row_UI object.
+    * @details Initializes the base `UI<char>` class with the welcome message "Welcome to Four in a row game".
+    */
     Four_in_a_row_UI();
+
+    /**
+    * @brief Creates a player of the specified type.
+    * @param name Name of the player.
+    * @param symbol Character symbol ('X' or 'O') assigned to the player.
+    * @param type The type of the player (Human or Computer).
+    * @return Pointer to the newly created Player<char> instance.
+    */
     Player<char> *create_player(string &name, char symbol, PlayerType type);
+
+    /**
+    * @brief Retrieves the next move from a player.
+    * @details
+    * - Human: Prompts for y coordinates.
+    * - Computer: Generates random column and get the available x from it to get a valid move.
+    * @param player Pointer to the player whose move is being requested.
+    * @return A pointer to a new `Move<char>` object representing the player's action.
+    */
     virtual Move<char> *get_move(Player<char> *player);
 };
--- games/SUS/SUS_Board.h
+++ games/SUS/SUS_Board.h
@@ -3,21 +3,76 @@
 
 using namespace std;
 
+/**
+ * @class SUS_Board
+ * @brief Represents the SUS game board.
+ *
+ * This class inherits from the generic `Board<char>` class and implements
+ * the specific logic required for the SUS (S,U) game, including
+ * move updates, win/draw detection, and adds a function to check number of SUS on board.
+ *
+ * @see Board
+ */
 class SUS_Board : public Board<char>
 {
 private:
-    char blank_symbol = '.';
+    char blank_symbol = '.'; ///< Character used to represent an empty cell on the board.
 
 public:
+    /**
+    * @brief Default constructor that initializes a 3x3 board.
+    */
     SUS_Board();
-    //count the number of SUS for player1
-    int SUSPlayer1 = 0;
-    //count the number of SUS for player2
-    int SUSPlayer2 = 0;
+
+    int SUSPlayer1 = 0;///< Counter for the total number of SUS made by player1.
+    int SUSPlayer2 = 0;///< Counter for the total number of SUS made by player2.
+
+    /**
+    * @brief Updates SUS count for this player.
+    * @details
+    * 1. Get the number of all SUS on board.
+    * 2. Check if this is player1(sym='S') or player2 (sym='U')
+    * 3. Update current player SUS count by subtracting player2 SUS count from all SUS count on board
+    * @param Player's symbol to check which player to update their SUS value .
+    * @return nothing.
+    */
     void checkNumOfSUS(char sym);
+
+    /**
+    * @brief Updates the board with a player's move.
+    * @details
+    * 1. Places the new symbol on the board.
+    * 2. update SUS for player1 and SUS for player2 with every move.
+    * @param move Pointer to a Move<char> object containing move coordinates and symbol.
+    * @return true if the move is valid and successfully applied, false otherwise.
+    */
     bool update_board(Move<char> *move);
+
+    /**
+    * @brief Checks when the board is full if the given player(player1 'S') has won the game.
+    * @param player Pointer to the player being checked(Always player1'S').
+    * @return true is player1 has more SUS than Player2.
+    */
     bool is_win(Player<char> *player);
+
+    /**
+    * @brief Checks if the game has ended in a draw.
+    * @param player Pointer to the player being checked.
+    * @return true if all cells are filled and number of SUS for player1=player2, false otherwise.
+    */
     bool is_draw(Player<char> *player);
+
+    /**
+    * @brief Checks when the board is full if the given player(player1 'S') has lost the game.
+    * @param player Pointer to the player being checked(Always player1'S').
+    * @return true is player1 has less SUS than Player2.
+    */
     bool is_lose(Player<char> *player);
+
+    /**
+    * @brief Determines if the game is over (win or draw).
+    * @param player Pointer to the player to evaluate.
+    * @return true if the game has ended, false otherwise.
+    */
     bool game_is_over(Player<char> *player);
 };
